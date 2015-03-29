f9a40668b99f4127267eaa4727e4e2e98a3bdd84
Ireneusz Bachanowicz <ireneusz.bachanowicz@gmail.com>
2015-03-29 17:11:43 +0000

    Added library export in transmission update script

--- Scripts/Transmission/updateXBMCLibrary.sh
+++ Scripts/Transmission/updateXBMCLibrary.sh
@@ -4,4 +4,8 @@ XBMC_PASS=xbmc
 XBMC_IP=192.168.0.126
 XBMC_PORT=8089
 
+# Library Scan
 curl --data-binary '{ "jsonrpc": "2.0", "method": "VideoLibrary.Scan"}'  -H 'content-type: application/json;'  http://$XBMC_USER:$XBMC_PASS@$XBMC_IP:$XBMC_PORT/jsonrpc
+
+# Library Export
+curl --data-binary '{ "jsonrpc": "2.0", "method": "VideoLibrary.Export", "params": {"options": { "images": true} } }' -H 'content-type: application/json;'  http://$XBMC_USER:$XBMC_PASS@$XBMC_IP:$XBMC_PORT/jsonrpc
